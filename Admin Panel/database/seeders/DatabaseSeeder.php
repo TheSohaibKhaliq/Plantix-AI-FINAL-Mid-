2e97f4f80eb534445f87724ea2d77cb9b4e81701
ABEERANOOR2202 <abeeranoor2202@gmail.com>
2026-02-24 22:24:06 +0500

    Refactor DatabaseSeeder to include detailed comments on seeder dependencies

--- Admin Panel/database/seeders/DatabaseSeeder.php
+++ Admin Panel/database/seeders/DatabaseSeeder.php
@@ -9,10 +9,33 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      *
+     * Each seeder is called in dependency order:
+     *  1. Users            – required by all other seeders
+     *  2. Zones            – required by vendors
+     *  3. Categories       – required by vendors & products
+     *  4. Roles/Perms      – assigns role_id to admin users
+     *  5. Vendors          – required by products, coupons, orders
+     *  6. Products         – required by orders
+     *  7. Taxes & Coupons  – coupons reference vendors
+     *  8. Orders           – references users, vendors, products, coupons
+     *  9. Wallet/Payouts   – references users, vendors, orders
+     * 10. Misc             – gift cards, on-board slides, store filters, currencies, CMS
+     *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            UsersSeeder::class,
+            ZonesSeeder::class,
+            CategoriesSeeder::class,
+            RolesPermissionsSeeder::class,
+            VendorsSeeder::class,
+            ProductsSeeder::class,
+            TaxesAndCouponsSeeder::class,
+            OrdersSeeder::class,
+            WalletAndPayoutsSeeder::class,
+            MiscSeeder::class,
+        ]);
     }
 }
